cfe4cb898bd3291319a832d0ee2f5b6d958683e9
abhay kumar <abhaykr95678@gmail.com>
2023-07-24 22:37:09 +0530

    updatated

--- src/Common/TabNavigator.js
+++ src/Common/TabNavigator.js
@@ -1,40 +1,45 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import OurProducts from "../Screens/Authenticated/OurProducts";
 import Icon from "react-native-vector-icons/FontAwesome";
-const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarStyle: {
-          backgroundColor: "#131636",
-          paddingBottom: 20,
-          height: 85,
-          bottom: 0,
-        },
-      }}
-    >
-      <Tab.Screen
-        name="Products"
-        component={OurProducts}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ focused }) => (
-            <Icon
-              name="user-circle"
-              size={25}
-              color={focused ? "blue" : "white"}
-            />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <View style={styles.TabNavigator}>
+      <View style={styles.TabIcon}>
+        <Icon name="home" size={28} color="#ddd9d9" />
+        <Text style={{ color: "#ddd9d9" }}>Home</Text>
+      </View>
+      <View style={styles.TabIcon}>
+        <Icon name="shopping-cart" size={28} color="#ddd9d9" />
+        <Text style={styles.TabIcon}>Product</Text>
+      </View>
+      <View style={styles.TabIcon}>
+        <Icon name="user" size={28} color="#ddd9d9" />
+        <Text style={styles.TabIcon}>Product</Text>
+      </View>
+      <View style={styles.TabIcon}>
+        <Icon name="list-ul" size={28} color="#ddd9d9" />
+        <Text style={styles.TabIcon}>More</Text>
+      </View>
+    </View>
   );
 };
 
 export default TabNavigator;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  TabNavigator: {
+    backgroundColor: "#242222",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingTop: 16,
+    paddingBottom: 16,
+    position: "absolute",
+    width: "100%",
+    bottom: 0,
+  },
+  TabIcon: {
+    color: "#ddd9d9",
+    alignItems: "center",
+  },
+});
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -13,6 +13,7 @@ import img1 from "../../assets/heading1.webp";
 import img2 from "../../assets/heading2.jpg";
 import img3 from "../../assets/heading3.jpg";
 import { Heading1, Heading2, Heading3, ImageList } from "../DataList";
+import TabNavigator from "../Common/TabNavigator";
 
 const Home = ({ navigation }) => {
   const [showMore1, setShowMore1] = useState("");
@@ -20,95 +21,110 @@ const Home = ({ navigation }) => {
   const [showMore3, setShowMore3] = useState("");
 
   return (
-    <ScrollView>
-      <View style={{ height: "100%" }}>
-        <View style={styles.seacrchArea}>
-          <Text style={styles.heading}>
-            Find The Perfect Freelance Service For Your Business
-          </Text>
-          <View style={styles.searchButton}>
-            <TextInput style={styles.CInput} placeholder="Search" />
-            <TouchableOpacity style={styles.searchbtn}>
-              <Text style={styles.textbtn}>SEARCH</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <FlatList
-          style={styles.imageList}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          data={ImageList}
-          renderItem={({ item }) => {
-            return <Image style={styles.image} source={item.img_url} />;
-          }}
-          keyExtractor={(item) => item.id}
-        />
-        <View style={{ marginTop: 10 }}>
-          <View style={styles.firstImaheInfo}>
-            <Image style={styles.ImgStyle} source={img1} resizeMode="contain" />
-          </View>
-          <View style={{ padding: 10 }}>
-            <Text style={styles.paraHeading}>Plagiarism Checking</Text>
-            <Text style={styles.paragraph}>
-              {showMore1 ? Heading1 : `${Heading1.substring(0, 118)}`}
+    <View>
+      <ScrollView>
+        <View>
+          <View style={styles.seacrchArea}>
+            <Text style={styles.heading}>
+              Find The Perfect Freelance Service For Your Business
             </Text>
+            <View style={styles.searchButton}>
+              <TextInput style={styles.CInput} placeholder="Search" />
+              <TouchableOpacity style={styles.searchbtn}>
+                <Text style={styles.textbtn}>SEARCH</Text>
+              </TouchableOpacity>
+            </View>
           </View>
+          <FlatList
+            style={styles.imageList}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={ImageList}
+            renderItem={({ item }) => {
+              return <Image style={styles.image} source={item.img_url} />;
+            }}
+            keyExtractor={(item) => item.id}
+          />
+          <View style={{ marginTop: 10 }}>
+            <View style={styles.firstImaheInfo}>
+              <Image
+                style={styles.ImgStyle}
+                source={img1}
+                resizeMode="contain"
+              />
+            </View>
+            <View style={{ padding: 10 }}>
+              <Text style={styles.paraHeading}>Plagiarism Checking</Text>
+              <Text style={styles.paragraph}>
+                {showMore1 ? Heading1 : `${Heading1.substring(0, 118)}`}
+              </Text>
+            </View>
 
-          <View style={styles.showDetails}>
-            <TouchableOpacity
-              style={styles.CButton}
-              onPress={() => setShowMore1(!showMore1)}
-            >
-              <Text style={styles.btnText}>
-                {showMore1 ? "Hide Details" : "View Details"}
+            <View style={styles.showDetails}>
+              <TouchableOpacity
+                style={styles.CButton}
+                onPress={() => setShowMore1(!showMore1)}
+              >
+                <Text style={styles.btnText}>
+                  {showMore1 ? "Hide Details" : "View Details"}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={{ marginTop: 10 }}>
+            <View style={styles.firstImaheInfo}>
+              <Image
+                style={styles.ImgStyle}
+                source={img2}
+                resizeMode="contain"
+              />
+            </View>
+            <View style={{ padding: 10 }}>
+              <Text style={styles.paraHeading}>Writing Enhancements</Text>
+              <Text style={styles.paragraph}>
+                {showMore2 ? Heading2 : `${Heading1.substring(0, 118)}`}
               </Text>
-            </TouchableOpacity>
+            </View>
+            <View style={styles.showDetails}>
+              <TouchableOpacity
+                style={styles.CButton}
+                onPress={() => setShowMore2(!showMore2)}
+              >
+                <Text style={styles.btnText}>
+                  {showMore2 ? "Hide Details" : "View Details"}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={{ marginTop: 10, marginBottom: "22%" }}>
+            <View style={styles.firstImaheInfo}>
+              <Image
+                style={styles.ImgStyle}
+                source={img3}
+                resizeMode="contain"
+              />
+            </View>
+            <View style={{ padding: 10 }}>
+              <Text style={styles.paraHeading}>Why Use a Plagiarism ?</Text>
+              <Text style={styles.paragraph}>
+                {showMore3 ? Heading3 : `${Heading1.substring(0, 118)}`}
+              </Text>
+            </View>
+            <View style={styles.showDetails}>
+              <TouchableOpacity
+                style={styles.CButton}
+                onPress={() => setShowMore3(!showMore3)}
+              >
+                <Text style={styles.btnText}>
+                  {showMore3 ? "Hide Details" : "View Details"}
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
-        <View style={{ marginTop: 10 }}>
-          <View style={styles.firstImaheInfo}>
-            <Image style={styles.ImgStyle} source={img2} resizeMode="contain" />
-          </View>
-          <View style={{ padding: 10 }}>
-            <Text style={styles.paraHeading}>Writing Enhancements</Text>
-            <Text style={styles.paragraph}>
-              {showMore2 ? Heading2 : `${Heading1.substring(0, 118)}`}
-            </Text>
-          </View>
-          <View style={styles.showDetails}>
-            <TouchableOpacity
-              style={styles.CButton}
-              onPress={() => setShowMore2(!showMore2)}
-            >
-              <Text style={styles.btnText}>
-                {showMore2 ? "Hide Details" : "View Details"}
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <View style={{ marginTop: 10, marginBottom: "10%" }}>
-          <View style={styles.firstImaheInfo}>
-            <Image style={styles.ImgStyle} source={img3} resizeMode="contain" />
-          </View>
-          <View style={{ padding: 10 }}>
-            <Text style={styles.paraHeading}>Why Use a Plagiarism ?</Text>
-            <Text style={styles.paragraph}>
-              {showMore3 ? Heading3 : `${Heading1.substring(0, 118)}`}
-            </Text>
-          </View>
-          <View style={styles.showDetails}>
-            <TouchableOpacity
-              style={styles.CButton}
-              onPress={() => setShowMore3(!showMore3)}
-            >
-              <Text style={styles.btnText}>
-                {showMore3 ? "Hide Details" : "View Details"}
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+      <TabNavigator />
+    </View>
   );
 };
 
